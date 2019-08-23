74cb7c69075e56c3f4401eff3b20d1ceee8f680c
Yeison Tapia <yftapial@ut.edu.co>
2019-08-23 17:45:31 -0500

    + Add ConversationUser resource

---
+++ Entities/ConversationUser.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Modules\Ichat\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ConversationUser extends Model
+{
+  protected $table = 'ichat__conversation_user';
+
+  protected $fillable = [
+    'conversation_id',
+    'user_id',
+    'last_message_readed',
+  ];
+
+  public function conversation()
+  {
+    return $this->belongsTo('Modules\Ichat\Entities\Conversation', 'conversation_id');
+  }
+
+  public function user()
+  {
+    $driver = config('asgard.user.config.driver');
+    return $this->belongsTo("Modules\\User\\Entities\\{$driver}\\User", 'user_id');
+  }
+
+}
---
+++ Transformers/ConversationUserTransformer.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Modules\Ichat\Transformers;
+
+use Illuminate\Http\Resources\Json\Resource;
+use Modules\Iprofile\Transformers\UserTransformer;
+use Illuminate\Support\Facades\Auth;
+
+class ConversationUserTransformer extends Resource
+{
+  public function toArray($request)
+  {
+    $data = [
+      'id' => $this->id,
+      'conversationId' => $this->when( $this->conversation_id, $this->conversation_id ),
+      'userId' => $this->when( $this->user_id, $this->user_id ),
+      'lastMessageReaded' => $this->last_message_readed,
+      'user' => new UserTransformer ( $this->whenLoaded('user') ),
+      'conversation' => new ConversationTransformer ( $this->whenLoaded('conversation') ),
+
+    ];
+    return $data;
+  }
+}
