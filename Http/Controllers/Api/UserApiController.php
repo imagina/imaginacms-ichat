0e23802dfd0bd1195bc78a56d53288561528edce
Yeison Tapia <yftapial@ut.edu.co>
2019-08-23 17:46:17 -0500

    + Add User resource (partial)

---
+++ Http/Controllers/Api/UserApiController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Modules\Ichat\Http\Controllers\Api;
+
+// Requests & Response
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+// Base Api
+use Modules\Ihelpers\Http\Controllers\Api\BaseApiController;
+
+// Transformers
+use Modules\Ichat\Transformers\UserTransformer;
+
+// Repositories
+use Modules\Ichat\Repositories\UserRepository;
+
+use Illuminate\Support\Facades\Auth;
+
+class UserApiController extends BaseApiController
+{
+  private $user;
+  public function __construct(UserRepository $user)
+  {
+    $this->user = $user;
+  }
+
+  /**
+   * GET ITEMS
+   *
+   * @return mixed
+   */
+  public function index(Request $request)
+  {
+    try {
+      //Get Parameters from URL.
+      $params = $this->getParamsRequest($request);
+      //Request to Repository
+      $data = $this->user->getItemsBy($params);
+      //Response
+      $response = ["data" => UserTransformer::collection($data)];
+      //If request pagination add meta-page
+      $params->page ? $response["meta"] = ["page" => $this->pageTransformer($data)] : false;
+    } catch (\Exception $e) {
+      $status = $this->getStatusError($e->getCode());
+      $response = ["errors" => $e->getMessage()];
+    }
+    //Return response
+    return response()->json($response ?? ["data" => "Request successful"], $status ?? 200);
+  }
+
+  /**
+   * GET A ITEM
+   *
+   * @param $criteria
+   * @return mixed
+   */
+  public function show($criteria, Request $request)
+  {
+    try {
+      //Get Parameters from URL.
+      $params = $this->getParamsRequest($request);
+      //Request to Repository
+      $data = $this->user->getItem($criteria, $params);
+      //Break if no found item
+      if (!$data) throw new Exception('Item not found', 204);
+      //Response
+      $response = ["data" => new UserTransformer($data)];
+      //If request pagination add meta-page
+      $params->page ? $response["meta"] = ["page" => $this->pageTransformer($data)] : false;
+    } catch (\Exception $e) {
+      $status = $this->getStatusError($e->getCode());
+      $response = ["errors" => $e->getMessage()];
+    }
+    //Return response
+    return response()->json($response ?? ["data" => "Request successful"], $status ?? 200);
+  }
+
+}
