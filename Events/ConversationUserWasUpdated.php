8cd5484c0f85ff961def80c908e1da2c6b9b13b9
Yeison Tapia <yftapial@ut.edu.co>
2019-08-30 11:13:37 -0500

    + Add implements in conversation user was Update, emit event for each user of this conversations

--- Events/ConversationUserWasUpdated.php
+++ Events/ConversationUserWasUpdated.php
@@ -2,29 +2,50 @@
 
 namespace Modules\Ichat\Events;
 
+use Illuminate\Broadcasting\Channel;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
-class ConversationUserWasUpdated
+class ConversationUserWasUpdated implements ShouldBroadcast
 {
-    use SerializesModels;
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $user_id;
+    public $message;
 
     /**
      * Create a new event instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($user_id, $message)
     {
-        //
+      $this->user_id = $user_id;
+      $this->message = $message;
     }
 
-    /**
+    public function broadcastAs()
+    {
+        return "conversationsUserUpdated$this->user_id";
+    }
+
+    public function broadcastWith()
+    {
+        return [
+            'id' => $this->message
+        ];
+    }
+
+
+  /**
      * Get the channels the event should be broadcast on.
      *
      * @return array
      */
     public function broadcastOn()
     {
-        return [];
+      return new Channel('global');
     }
 }
