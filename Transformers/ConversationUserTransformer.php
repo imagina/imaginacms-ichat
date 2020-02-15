5267259455c39d2fe6852701c77a26ab9c1ab74c
YeisonTapia <yftapial@ut.edu.co>
2020-02-15 12:35:39 -0500

    + Remove whenLoaded in user field into conversationUserTransformer.

--- Transformers/ConversationUserTransformer.php
+++ Transformers/ConversationUserTransformer.php
@@ -15,7 +15,7 @@ class ConversationUserTransformer extends Resource
       'conversationId' => $this->when( $this->conversation_id, $this->conversation_id ),
       'userId' => $this->when( $this->user_id, $this->user_id ),
       'lastMessageReaded' => $this->last_message_readed,
-      'user' => new UserTransformer ( $this->whenLoaded('user') ),
+      'user' => new UserTransformer ( $this->user ),
       'conversation' => new ConversationTransformer ( $this->whenLoaded('conversation') ),
 
     ];
