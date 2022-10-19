38018498e405df73f68a52581579ca1fea788bd3
msolano <msolano@imaginacolombia.com>
2022-10-19 12:10:26 -0500

    +Update: MessageApiController to use first_name from params

--- Http/Controllers/Api/MessageApiController.php
+++ Http/Controllers/Api/MessageApiController.php
@@ -44,6 +44,7 @@ class MessageApiController extends BaseApiController
         "provider" => $data["provider"] ?? null,
         "recipient_id" => $data["recipient_id"] ?? null,
         "sender_id" => $data["user_id"] ?? $data["sender_id"] ?? null,
+        "first_name" => $data["first_name"] ?? null,
         "conversation_id" => $data["conversation_id"] ?? null,
         "conversation_private" => $data["conversation_private"] ?? 1,
         "media_id" => $data["media_id"] ?? $data["attached"] ?? null,
