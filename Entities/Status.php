9b0ed5c1de34ad938faab500d268c7d3970a9fc4
Nicolas <nmontealegre@imaginacolombia.com>
2023-09-14 16:52:31 -0500

    Add states from WhatsApp integration

--- Entities/Status.php
+++ Entities/Status.php
@@ -8,8 +8,21 @@ class Status
     const SENT = 1;
     const DELIVERED = 2;
     const READ = 3;
-    const FAILED = 4;
-    const OBTAINED = 5;
+    const OBTAINED = 4;
+    const TIMEEXCEEDED = 5;
+    const MEDIADOWNLOADERROR = 6;
+    const INCORRECTTEMPLATEPARAM = 7;
+    const TEMPLATENOTFOUND = 8;
+    const TEMPLATEFORMATTINGERROR = 9;
+    const TEMPLATEPAUSED = 10;
+    const TEMPLATEDISABLED = 11;
+    const TIMEOUT = 12;
+    const UNKNOWNERROR = 13;
+    const MESSAGENOTSENT = 14;
+    const INCOMPATIBLEMSGTYPE = 15;
+    const MEDIAUPLOADERROR = 16;
+    const SERVERUNAVAILABLE = 17;
+    const UNIDENTIFIEDERROR = 18;
 
 
     private $statuses = [];
@@ -20,8 +33,20 @@ class Status
             self::SENT => trans('ichat::messages.status.sent'),
             self::DELIVERED => trans('ichat::messages.status.delivered'),
             self::READ => trans('ichat::messages.status.read'),
-            self::FAILED => trans('ichat::messages.status.failed'),
-            self::OBTAINED => trans('ichat::messages.status.obtained')
+            self::OBTAINED => trans('ichat::messages.status.obtained'),
+            self::TIMEEXCEEDED => trans('ichat::messages.status.timeExceeded'),
+            self::MEDIADOWNLOADERROR => trans('ichat::messages.status.mediaDownloadError'),
+            self::INCORRECTTEMPLATEPARAM => trans('ichat::messages.status.incorrectTemplateParam'),
+            self::TEMPLATENOTFOUND => trans('ichat::messages.status.templateNotFound'),
+            self::TEMPLATEFORMATTINGERROR => trans('ichat::messages.status.templateFormattingError'),
+            self::TEMPLATEPAUSED => trans('ichat::messages.status.templatePaused'),
+            self::TEMPLATEDISABLED => trans('ichat::messages.status.templateDisabled'),
+            self::TIMEOUT => trans('ichat::messages.status.timeout'),
+            self::UNKNOWNERROR => trans('ichat::messages.status.unknownError'),
+            self::MESSAGENOTSENT => trans('ichat::messages.status.messageNotSent'),
+            self::INCOMPATIBLEMSGTYPE => trans('ichat::messages.status.incompatibleMsgType'),
+            self::SERVERUNAVAILABLE => trans('ichat::messages.status.serverUnavailable'),
+            self::UNIDENTIFIEDERROR => trans('ichat::messages.status.unidentifiedError')
         ];
     }
 
--- Resources/lang/es/messages.php
+++ Resources/lang/es/messages.php
@@ -29,6 +29,19 @@ return [
         'delivered' => 'Entregado',
         'read' => 'Leído',
         'failed' => 'Fallido',
-        'obtained' => 'Obtenido'
+        'obtained' => 'Obtenido',
+        'timeExceeded' => 'Pasaron más de 24 horas desde la última respuesta del destinatario.',
+        'mediaDownloadError' => 'Fallo en la descarga del archivo multimedia adjunto.',
+        'incorrectTemplateParam' => 'Fallo por discrepancia en los parámetros de la plantilla.',
+        'templateNotFound' => 'Plantilla inexistente en el idioma o no aprobada.',
+        'templateFormattingError' => 'Plantilla incumple políticas de formato.',
+        'templatePaused' => 'Plantilla temporalmente desactivada.',
+        'templateDisabled' => 'Plantilla ha sido desactivada permanentemente.',
+        'timeout' => 'Operación retrasada por inactividad o sobrecarga del sistema.',
+        'unknownError' => 'Fallo debido a un problema imprevisto o no identificado.',
+        'messageNotSent' => 'Fallo en el envío del mensaje por causas no detalladas.',
+        'incompatibleMsgType' => 'Error debido a incompatibilidad del tipo de mensaje.',
+        'serverUnavailable' => 'Servidor temporalmente no disponible.',
+        'unidentifiedError' => 'Error no identificado.'
     ]
 ];
