2720c4f6256ea4b431dfbfa8023ba1c678ef600d
William Verde <williamverde10@gmail.com>
2024-03-14 17:46:23 -0400

    Added: new translations

--- Resources/lang/en/common.php
+++ Resources/lang/en/common.php
@@ -7,5 +7,11 @@ return [
   'errors' => [
     'providerDisable' => 'Notification Provider Is Currently Disabled',
   ],
+  'conversation' => [
+    'created' => [
+      'title' => 'A new conversation has been created',
+      'message' => ':user has started a new conversation with you',
+    ]
+  ],
 ];
 
