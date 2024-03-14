5f864c79f5aa7f1e4e11b050c8536a77dce4b363
William Verde <williamverde10@gmail.com>
2024-03-14 17:54:57 -0400

    Added: translation setting in english

--- Resources/lang/en/common.php
+++ Resources/lang/en/common.php
@@ -4,6 +4,9 @@ return [
   'settings' => [
     'labelExternalRoles' => 'External Roles',
   ],
+  'settingsHelp' => [
+    'responsibleUsers' => 'The users selected here will be responsible for attending to pending conversations...',
+  ],
   'errors' => [
     'providerDisable' => 'Notification Provider Is Currently Disabled',
   ],
