6ed2df360b8f0971bddff7d75b797475281fbd6a
William Verde <williamverde10@gmail.com>
2024-03-14 18:10:28 -0400

    Added: notification process when create a conversation

--- Services/ConversationService.php
+++ Services/ConversationService.php
@@ -5,6 +5,7 @@ namespace Modules\Ichat\Services;
 
 
 use Modules\Ichat\Repositories\ConversationRepository;
+use Modules\User\Entities\Sentinel\User;
 
 class ConversationService
 {
@@ -29,4 +30,53 @@ class ConversationService
         }catch (\Exception $e){
         }
     }
+
+    /**
+    * Get emails and broadcast information
+    */
+    public function getEmailsAndBroadcast($conversation)
+    {
+        
+        $emailTo = [];
+        $broadcastTo = [];
+        $createdByUser = $conversation->createdByUser->present()->fullname;
+
+        //Case | User
+        if($conversation->entity_type=="Modules\User\Entities\Sentinel\User"){
+            
+           $userLogged = \Auth::user();
+
+           //Same User Logged with Conversation User Id
+           if($userLogged->id==$conversation->entity_id){
+                $usersToNotify = json_decode(setting("ichat::responsableUsers"));
+                if(count($usersToNotify)>0){
+                    //Setting Infor
+                    $users = User::whereIn("id", $usersToNotify)->get();
+                    $emailTo = array_merge($emailTo, $users->pluck('email')->toArray());
+                    $broadcastTo = $users->pluck('id')->toArray();
+
+                }else{
+                    //A la Tienda que le estan escribiendo
+                    if(isset($conversation->organization) && !is_null($conversation->organization)){
+                        $userOrg = $conversation->organization->users->first();
+                        array_push($emailTo, $userOrg->email);
+                        array_push($broadcastTo, $userOrg->id);
+                    }
+                }
+           }else{
+                //Notificar al Entity Id de Conversation
+                array_push($emailTo, $conversation->entity->email);
+                array_push($broadcastTo, $conversation->entity_id);
+           }
+
+        }
+
+        // Data Notification
+        $to["email"] = $emailTo;
+        $to["broadcast"] = $broadcastTo;
+        $to['createdByUser'] = $createdByUser;
+    
+        return $to;
+    }
+    
 }
